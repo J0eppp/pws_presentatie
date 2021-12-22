bd22d82396b782cb1df2daaf3f6d08bdcafd3594
J0eppp <joep.van.dijk04@gmail.com>
2021-12-22 19:50:32 +0100

    Add begin of presentation

--- scenes.py
+++ scenes.py
@@ -1,6 +1,63 @@
 from manimlib import *
 
 
+class Presentation(Scene):
+    def construct(self):
+        # Intro dingen
+        title = Text(
+            "Welkom bij onze presentatie over:").to_edge(UP)
+        under_title = Text(
+            "Profielwerkstuk roosteralgoritmiek").to_edge(UP * 2.3)
+        self.play(Write(title))
+        self.play(Write(under_title))
+
+        by = Text("Door: Joep van Dijk en Sam Staijen").to_edge(
+            UP * 4).scale(.6)
+        self.play(Write(by))
+
+        mentor = Text("Begeleider: T. Nooijen").to_edge(UP * 5).scale(.5)
+        self.play(Write(mentor))
+
+        subject = Text("Vak: wiskunde").to_edge(UP * 6).scale(.4)
+        self.play(Write(subject))
+
+        self.wait(1)
+
+        self.play(FadeOut(subject), FadeOut(mentor), FadeOut(
+            by), FadeOut(under_title), FadeOut(title))
+
+        # Wat is het roosterprobleem?
+        title = Text("Wat is het roosterprobleem?").to_edge(UP)
+        self.play(Write(title))
+
+        timetabling_problem = Text(
+            "Een rooster moet aan een aantal eisen voldoen:").to_edge(UP * 2.3).scale(.9)
+        self.play(Write(timetabling_problem))
+        tp_1 = Text(
+            "1. Alle lessen die een leerling hoort te krijgen moeten gegeven worden").to_edge(UP * 4).scale(.6).to_edge(LEFT)
+        self.play(Write(tp_1))
+        tp_2 = Text(
+            "2. Een leerling of docent kan niet twee lessen tegelijk volgen/geven").to_edge(UP * 6).scale(.6).to_edge(LEFT)
+        self.play(Write(tp_2))
+        tp_3 = Text(
+            "3. Veel docenten werken part-time").to_edge(UP * 8).scale(.6).to_edge(LEFT)
+        self.play(Write(tp_3))
+
+        self.wait(1)
+
+        timetabling_preferences = Text(
+            "Daarnaast zullen er persoonlijke belangen zijn:").to_edge(UP * 2.3).scale(.9)
+        self.play(FadeOut(tp_1),
+                  FadeOut(tp_2), FadeOut(tp_3), FadeOut(timetabling_problem))
+        self.play(Write(timetabling_preferences))
+        tpr_1 = Text("1. Leerlingen en docenten zullen zelf voorkeuren hebben, zoals zo min mogelijk tussenuren").to_edge(
+            UP * 4).scale(.6).to_edge(LEFT)
+        self.play(Write(tpr_1))
+        tpr_2 = Text("2. Leerlingen hebben liever dat de vakken verspreid zijn over de week, je wilt niet 4 uur Nederlands op één dag hebben").to_edge(
+            UP * 6).scale(.6).to_edge(LEFT)
+        self.play(Write(tpr_2))
+
+
 class SimplexExplanation(Scene):
     def construct(self):
         intro = Text("Het simplexalgoritme\nMet het timmermanprobleem")
@@ -18,10 +75,30 @@ class SimplexExplanation(Scene):
         self.play(FadeTransform(intro, timmerman_uitleg_1))
         self.wait(2)
 
+        timmerman_obj_func_text = Text(
+            "Dit kunnen we formuleren in de volgende formule:").to_edge(UP)
+        self.play(FadeTransform(timmerman_uitleg_1, timmerman_obj_func_text))
+        self.wait(1)
+
+        timmerman_obj_func = Tex(
+            "f(x, y) = 180 \cdot x + 200 \cdot y").to_edge(UP * 3)
+        timmerman_obj_func_expl = Text(
+            "Waarbij x het aantal tafels is en y het aantal boekenkasten").to_edge(UP * 5).scale(0.8)
+        self.play(ShowCreation(timmerman_obj_func))
+        self.play(ShowCreation(timmerman_obj_func_expl))
+
+        self.wait(2)
+
+        # self.remove(*timmerman_obj_func_text)
+        self.remove(*timmerman_obj_func)
+        self.remove(*timmerman_obj_func_expl)
+        self.play(FadeOut(timmerman_obj_func),
+                  FadeOut(timmerman_obj_func_expl))
+
         timmerman_uitleg_2 = Text(
             "Een boekenkast kost 20 eenheden hout en 4 uur aan werktijd").to_edge(UP)
 
-        self.play(FadeTransform(timmerman_uitleg_1, timmerman_uitleg_2))
+        self.play(FadeTransform(timmerman_obj_func_text, timmerman_uitleg_2))
         self.wait(2)
 
         timmerman_uitleg_3 = Text(
@@ -38,8 +115,6 @@ class SimplexExplanation(Scene):
                          "include_numbers": True}
         )
 
-        self.play(ShowCreation(axes))
-
         # 5x + 4y = 80
         # 4y = 80 - 5x
         # y = 20 - 5/4 x
@@ -50,6 +125,8 @@ class SimplexExplanation(Scene):
         graph1_text.scale(0.8)
         self.play(FadeTransform(timmerman_uitleg_3, graph1_text))
         self.wait(2)
+
+        self.play(ShowCreation(axes))
 
         graph1_formula = Tex("5 \cdot x + 4 \cdot y = 80").to_edge(UP * 3)
 
@@ -68,12 +145,14 @@ class SimplexExplanation(Scene):
                                 x_range=[0, 20], color=RED)
         graph2_text = Text(
             "De timmerman mag niet teveel hout gebruiken", ).to_edge(UP).scale(0.8)
-
-        self.play(FadeTransform(graph1_text, graph2_text))
-
         graph2_formula = Tex("10 \cdot x + 20 \cdot y = 200").to_edge(UP * 3)
-        self.play(ShowCreation(graph2), FadeTransform(
+
+        self.play(FadeTransform(graph1_text, graph2_text), FadeTransform(
             graph1_formula, graph2_formula))
+
+        self.wait(1)
+
+        self.play(ShowCreation(graph2))
 
         self.wait(2)
 
