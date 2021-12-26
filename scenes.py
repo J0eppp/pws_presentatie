4a567da7b9f9e61c48480246c67529051cef89d2
sam32123 <samstaijen@gmail.com>
2021-12-26 14:29:02 +0100

    added 2 chapters
    resultaten+conclusie
    
    discussie

--- scenes.py
+++ scenes.py
@@ -5,16 +5,13 @@ import random
 
 class Presentation(Scene):
     def construct(self):
-        # Intro dingen
         title = Text(
-            "Welkom bij onze presentatie over:").to_edge(UP)
+           "Welkom bij onze presentatie over:").to_edge(UP)
         under_title = Text(
             "Profielwerkstuk roosteralgoritmiek").to_edge(UP * 2.3)
-        self.play(Write(title), Write(under_title))
-        # self.play(Write(under_title))
 
-        by = Text("Door: Joep van Dijk en Sam Staijen").to_edge(
-            UP * 6).scale(.6)
+        self.play(Write(title), Write(under_title) )
+        by = Text("Door: Joep van Dijk en Sam Staijen").to_edge(UP * 6).scale(.6)
         # self.play(Write(by))
 
         mentor = Text("Begeleider: T. Nooijen").to_edge(UP * 7).scale(.5)
@@ -42,8 +39,7 @@ class Presentation(Scene):
         osg.add(docent_part_time)
         self.play(Write(docent_part_time))
 
-        leerling_beschikbaar = Text(
-            "Een leerling moet op dat moment nog geen les hebben").to_edge(UP * 5).scale(.7)
+        leerling_beschikbaar = Text("Een leerling moet op dat moment nog geen les hebben").to_edge(UP * 5).scale(.7)
         osg.add(leerling_beschikbaar)
         self.play(Write(leerling_beschikbaar))
 
@@ -201,9 +197,151 @@ class Presentation(Scene):
         self.play(FadeOut(voorkeuren), FadeOut(voorkeuren_1),
                   FadeOut(voorkeuren_strafpunten))
 
-        # temp = Text(
-        #     "Docenten en leerlingen moeten op hetzelfde moment beschikbaar zijn").to_edge(UP * 3).scale(.8)
-        # self.play(Write(temp))
+        conclusie  = Text("Resultaten en Conclusie").to_edge(UP * 1)
+        self.play(Write(conclusie))
+
+        conclusie_1 = Text(
+            "Aantal tussenuren bij 2 docenten per vak").to_edge(UP * 3).scale(.7)
+        self.play(Write(conclusie_1))
+
+        self.wait(2)
+
+        staafdiagram1 = [0.88, 5.75, 3.00, 8.75, 2.67, 7.33]
+        inhoudstaafdiagram1 = [
+                "RS klas 1",
+                "LP klas 1",
+                "RS klas 2",
+                "LP klas 2",
+                "RS klas 3",
+                "LP klas 3",
+            ]
+        colors = ["#003f5c", "#58508d", "#003f5c", "#58508d","#003f5c", "#58508d", ]
+
+        bar = BarChart(
+            staafdiagram1,
+            max_value=max(staafdiagram1),
+            bar_colors=colors,
+            bar_names=inhoudstaafdiagram1,
+            bar_label_scale_val=.4,
+        ).to_edge(UP * 6).scale(1)
+        self.add(bar)
+
+        self.wait(2)
+
+        self.play(FadeOut(conclusie_1), FadeOut(bar))
+
+        conclusie_2 = Text(
+            "Aantal negende uren bij 2 docenten per vak").to_edge(UP * 3).scale(.7)
+        self.play(Write(conclusie_2))
+
+        self.wait(2)
+
+        staafdiagram2 = [3.0, 0.63, 2.75, 2.50, 3.00, 1.17]
+        inhoudstaafdiagram2 = [
+            "RS klas 1",
+            "LP klas 1",
+            "RS klas 2",
+            "LP klas 2",
+            "RS klas 3",
+            "LP klas 3",
+        ]
+        colors = ["#003f5c", "#58508d", "#003f5c", "#58508d", "#003f5c", "#58508d", ]
+
+        bar2 = BarChart(
+            staafdiagram2,
+            max_value=max(staafdiagram2),
+            bar_colors=colors,
+            bar_names=inhoudstaafdiagram2,
+            bar_label_scale_val=.4,
+        ).to_edge(UP * 6).scale(1)
+        self.add(bar2)
+
+        self.wait(2)
+        self.play(FadeOut(conclusie_2), FadeOut(bar2))
+
+        conclusie_3 = Text(
+            "Aantal tussenuren bij 3 docenten per vak").to_edge(UP * 3).scale(.7)
+        self.play(Write(conclusie_3))
+
+        self.wait(2)
+
+        staafdiagram3 = [0.38, 0.38, 1.75, 0.38, 3.00, 0.17]
+        inhoudstaafdiagram3 = [
+            "RS klas 1",
+            "LP klas 1",
+            "RS klas 2",
+            "LP klas 2",
+            "RS klas 3",
+            "LP klas 3",
+        ]
+        colors = ["#003f5c", "#58508d", "#003f5c", "#58508d", "#003f5c", "#58508d", ]
+
+        bar3 = BarChart(
+            staafdiagram3,
+            max_value=max(staafdiagram3),
+            bar_colors=colors,
+            bar_names=inhoudstaafdiagram3,
+            bar_label_scale_val=.4,
+        ).to_edge(UP * 6).scale(1)
+        self.add(bar3)
+
+        self.wait(2)
+
+        self.play(FadeOut(conclusie_3), FadeOut(bar3))
+
+        conclusie_4 = Text(
+            "Aantal negende uren bij 3 docenten per vak").to_edge(UP * 3).scale(.7)
+        self.play(Write(conclusie_4))
+
+        self.wait(2)
+
+        staafdiagram4 = [3.00, 0.00, 2.88, 0.00, 2.67, 0.00]
+        inhoudstaafdiagram4 = [
+            "RS klas 1",
+            "LP klas 1",
+            "RS klas 2",
+            "LP klas 2",
+            "RS klas 3",
+            "LP klas 3",
+        ]
+        colors = ["#003f5c", "#58508d", "#003f5c", "#58508d", "#003f5c", "#58508d", ]
+
+        bar4 = BarChart(
+            staafdiagram4,
+            max_value=max(staafdiagram4),
+            bar_colors=colors,
+            bar_names=inhoudstaafdiagram4,
+            bar_label_scale_val=.4,
+        ).to_edge(UP * 6).scale(1)
+        self.add(bar4)
+
+        self.wait(2)
+        self.play(FadeOut(conclusie), FadeOut(conclusie_4), FadeOut(bar4))
+
+        discussie = Text("Discussie").to_edge(UP * 1)
+        self.play(Write(discussie))
+
+        discussie_1 = Text(
+            "Maatschappelijk signiviciant, maar niet heel relevant").to_edge(UP * 3).scale(.7)
+        self.play(Write(discussie_1))
+
+        discussie_2 = Text(
+            "niet goed geweten wat te doen, indien betere inlezing mogelijk meer resultaat").to_edge(UP * 5).scale(.7)
+        self.play(Write(discussie_2))
+
+        discussie_3 = Text(
+            "Alleen onderbouw, geen bovenbouw").to_edge(UP * 8).scale(.7)
+        self.play(Write(discussie_3))
+
+        discussie_4 = Text(
+            "aanname docent is fulltimer en mag elk werkuur lesgeven").to_edge(UP * 10).scale(.7)
+        self.play(Write(discussie_4))
+
+        temp = Text(
+            "Docenten en leerlingen moeten op hetzelfde moment beschikbaar zijn").to_edge(UP * 3).scale(.8)
+        self.play(Write(temp))
+
+        #
         # on_screen.append(temp)
         # self.wait(1)
         # temp = Text("Een klas moet per vak dezelfde docent hebben").to_edge(
@@ -216,13 +354,13 @@ class Presentation(Scene):
         # self.play(Write(temp))
         # on_screen.append(temp)
         # self.wait(1)
-
+        #
         # for x in on_screen:
         #     self.play(FadeOut(x))
-
+        #
         # on_screen = []
         # timetable = Table([[]])
-
+        #
         # timetabling_problem = Text(
         #     "Een rooster moet aan een aantal eisen voldoen:").to_edge(UP * 2.3).scale(.9)
         # self.play(Write(timetabling_problem))
@@ -235,9 +373,9 @@ class Presentation(Scene):
         # tp_3 = Text(
         #     "3. Veel docenten werken part-time").to_edge(UP * 8).scale(.6).to_edge(LEFT)
         # self.play(Write(tp_3))
-
+        #
         # self.wait(1)
-
+        #
         # timetabling_preferences = Text(
         #     "Daarnaast zullen er persoonlijke belangen zijn:").to_edge(UP * 2.3).scale(.9)
         # self.play(FadeOut(tp_1),
@@ -249,122 +387,3 @@ class Presentation(Scene):
         # tpr_2 = Text("2. Leerlingen hebben liever dat de vakken verspreid zijn over de week, je wilt niet 4 uur Nederlands op één dag hebben").to_edge(
         #     UP * 6).scale(.6).to_edge(LEFT)
         # self.play(Write(tpr_2))
-
-
-class SimplexExplanation(Scene):
-    def construct(self):
-        intro = Text("Het simplexalgoritme\nMet het timmermanprobleem")
-
-        intro.to_edge(UP)
-
-        self.play(Write(intro))
-        self.wait(2)
-
-        timmerman_uitleg_1 = Text(
-            "Een timmerman kan tafels voor 180 euro verkopen en boekenkasten voor 200 euro")
-
-        timmerman_uitleg_1.to_edge(UP)
-
-        self.play(FadeTransform(intro, timmerman_uitleg_1))
-        self.wait(2)
-
-        timmerman_obj_func_text = Text(
-            "Dit kunnen we formuleren in de volgende formule:").to_edge(UP)
-        self.play(FadeTransform(timmerman_uitleg_1, timmerman_obj_func_text))
-        self.wait(1)
-
-        timmerman_obj_func = Tex(
-            "f(x, y) = 180 \cdot x + 200 \cdot y").to_edge(UP * 3)
-        timmerman_obj_func_expl = Text(
-            "Waarbij x het aantal tafels is en y het aantal boekenkasten").to_edge(UP * 5).scale(0.8)
-        self.play(ShowCreation(timmerman_obj_func))
-        self.play(ShowCreation(timmerman_obj_func_expl))
-
-        self.wait(2)
-
-        # self.remove(*timmerman_obj_func_text)
-        self.remove(*timmerman_obj_func)
-        self.remove(*timmerman_obj_func_expl)
-        self.play(FadeOut(timmerman_obj_func),
-                  FadeOut(timmerman_obj_func_expl))
-
-        timmerman_uitleg_2 = Text(
-            "Een boekenkast kost 20 eenheden hout en 4 uur aan werktijd").to_edge(UP)
-
-        self.play(FadeTransform(timmerman_obj_func_text, timmerman_uitleg_2))
-        self.wait(2)
-
-        timmerman_uitleg_3 = Text(
-            "Een tafel kost 10 eenheden hout en 5 uur aan werktijd").to_edge(UP)
-
-        self.play(FadeTransform(timmerman_uitleg_2, timmerman_uitleg_3))
-        self.wait(2)
-
-        # Create graph
-        axes = Axes(
-            x_range=[0, 25, 5],
-            y_range=[0, 25, 5],
-            axis_config={"color": BLUE,
-                         "include_numbers": True}
-        )
-
-        # 5x + 4y = 80
-        # 4y = 80 - 5x
-        # y = 20 - 5/4 x
-        graph1 = axes.get_graph(lambda x: 20 - 1.25 * x,
-                                color=WHITE, x_range=[0, 16])
-        graph1_text = Text(
-            "De timmerman mag niet meer dan 80 uur werken", ).to_edge(UP)
-        graph1_text.scale(0.8)
-        self.play(FadeTransform(timmerman_uitleg_3, graph1_text))
-        self.wait(2)
-
-        self.play(ShowCreation(axes))
-
-        graph1_formula = Tex("5 \cdot x + 4 \cdot y = 80").to_edge(UP * 3)
-
-        self.play(ShowCreation(graph1_formula))
-        self.wait(2)
-
-        self.play(ShowCreation(graph1))
-
-        self.wait(2)
-
-        # 10x + 20y = 200
-        # 20y = 200 - 10x
-        # 2y = 20 - x
-        # y = 10 - 1/2 x
-        graph2 = axes.get_graph(lambda x: 10 - 0.5 * x,
-                                x_range=[0, 20], color=RED)
-        graph2_text = Text(
-            "De timmerman mag niet teveel hout gebruiken", ).to_edge(UP).scale(0.8)
-        graph2_formula = Tex("10 \cdot x + 20 \cdot y = 200").to_edge(UP * 3)
-
-        self.play(FadeTransform(graph1_text, graph2_text), FadeTransform(
-            graph1_formula, graph2_formula))
-
-        self.wait(1)
-
-        self.play(ShowCreation(graph2))
-
-        self.wait(2)
-
-        point0 = Dot(color=YELLOW).move_to(
-            axes.coords_to_point(0, 0)).scale(0.5)
-        point1 = Dot(color=YELLOW).move_to(
-            axes.coords_to_point(13 + 1/3, 3 + 1/3)).scale(0.5)
-        point2 = Dot(color=YELLOW).move_to(
-            axes.coords_to_point(0, 10)).scale(0.5)
-        point3 = Dot(color=YELLOW).move_to(
-            axes.coords_to_point(0, 20)).scale(0.5)
-        point4 = Dot(color=YELLOW).move_to(
-            axes.coords_to_point(16, 0)).scale(0.5)
-        point5 = Dot(color=YELLOW).move_to(
-            axes.coords_to_point(20, 0)).scale(0.5)
-
-        self.play(ShowCreation(point0), ShowCreation(point1), ShowCreation(
-            point2), ShowCreation(point3), ShowCreation(point4), ShowCreation(point5))
-
-        dots_text = Text("Een van deze punten zal de maximale waarde zijn")
-
-        self.wait(2)
